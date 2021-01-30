d0a7e38dad44626376aff3c496c1c8f353f1318e
ACarrill42 <austincarrill@gmail.com>
2021-01-29 22:42:47 -0600

    save, fixing props

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -4,11 +4,16 @@ import './Comments.css';
 
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
-  const { comments } = props;
+  const { posts , setPosts } = props;
 
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {posts.map((comment) => {
+        return(
+          <Comment posts = {posts} setPosts = {setPosts} />
+        )
+      })}
     </div>
   );
 };
